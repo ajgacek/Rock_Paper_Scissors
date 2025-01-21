99b003a4da5beeb9b59a05f9b15689e19ab13298
ajgacek <ajgacek96@gmail.com>
2025-01-21 06:02:19 -0600

    Write logic to play a single round
    Write logic to play a single round, determining the winner of rock, paper, scissors.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,32 +1,90 @@
-let ComputerChoice;
+let computerChoice;
 getComputerChoice();
 
 function getComputerChoice() {
     random_number = Math.floor(Math.random() * 3);
 
     if(random_number == 0) {
-        ComputerChoice = "rock";
-        return ComputerChoice;
+        computerChoice = "rock";
+        return computerChoice;
     }
 
     else if(random_number == 1) {
-        ComputerChoice =  "paper";
-        return ComputerChoice;
+        computerChoice =  "paper";
+        return computerChoice;
     }
 
     else {
-        ComputerChoice = "scissors";
-        return ComputerChoice;
+        computerChoice = "scissors";
+        return computerChoice;
     }
 }
 
-console.log(ComputerChoice);
+console.log(computerChoice);
 
-let HumanChoice;
+let humanChoice;
 getHumanChoice();
 
 function getHumanChoice() {
-    HumanChoice = prompt("What's your choice?"); 
+    let human = prompt("What's your choice?");
+    humanChoice = human.toLowerCase(); 
 }
 
-console.log(HumanChoice); 
+console.log(humanChoice); 
+
+let humanScore = 0;
+let computerScore = 0; 
+
+playRound(humanChoice, computerChoice);
+
+function playRound (humanChoice, computerChoice) {
+
+    if (humanChoice == "rock") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("Tie! You both picked rock");
+                break;
+            case "paper":
+                console.log("You lose! Paper beats rock!");
+                computerScore++; 
+                break;
+            case "scissors":
+                console.log("You win! Rock beats paper");
+                humanScore++; 
+                break;
+        }
+    }
+
+    if (humanChoice == "paper") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You win! Paper beats rock!");
+                humanScore++;
+                break;
+            case "paper":
+                console.log("Tie! You both picked paper!");
+                break; 
+            case "scissors":
+                console.log("You lose! Scissors beats paper");
+                computerScore++; 
+                break;
+        }
+    }
+
+    if (humanChoice == "scissors") {
+        switch(computerChoice) {
+            case "rock":
+                console.log("You lose! Rock beats scissors!");
+                computerScore++; 
+                break;
+            case "paper":
+                console.log("You win! Scissors beats paper!"); 
+                humanScore++; 
+                break; 
+            case "scissors":
+                console.log("Tie! You both picked scissors!");
+                break;
+        }
+    }
+
+}
